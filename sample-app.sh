db65e66a3c49533bcaf26d479f69404814228505
Bencitoo <112555352+Bencitoo@users.noreply.github.com>
2023-10-18 01:21:30 +0800

    sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -22,5 +22,4 @@ echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 cd tempdir
 docker build -t sampleapp .
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
-docker ps -a 
-bebebebe
+docker ps -a
